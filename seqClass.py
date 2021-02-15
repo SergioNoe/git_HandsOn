701323aa9070a00058a85f6700cdc4e8a05e2977
Sergio Noe <sergionoe0@gmail.com>
2021-02-15 17:58:56 +0100

    Comments in seqClass.py

--- seqClass.py
+++ seqClass.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 
+# Imports
 import sys, re
 from argparse import ArgumentParser
 
+# Command arguments
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
 parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
 
+# Show help in command
 if len(sys.argv) == 1:
     parser.print_help()
     sys.exit(1)
 
+# Obtaining arguments passed in command 
 args = parser.parse_args()
 
+# Avoiding lower case letters
 args.seq = args.seq.upper()
+
+# Check sequence type
 if re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):
         print ('The sequence is DNA')
@@ -24,6 +31,7 @@ if re.search('^[ACGTU]+$', args.seq):
 else:
     print ('The sequence is not DNA nor RNA')
 
+# Search for a motif inside the sequence
 if args.motif:
   args.motif = args.motif.upper()
   print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
